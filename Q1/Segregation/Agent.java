62223ed51d9d5e1e746319a22ef6bc48fb997e15
halilberkdergi <halilberkdergi@gmail.com>
2022-02-26 01:43:58 +0300

    Yeni başlanndı. Devam edilecek

---
+++ Q1/Segregation/Agent.java
@@ -0,0 +1,16 @@
+package Segregation;
+import java.util.*;
+import java.lang.System;
+
+public class Agent {
+
+    private final int x;
+    private final int y;
+
+    public Agent(int X, int Y)
+    {
+        this.x=X;
+        this.y=Y;
+    }
+
+}

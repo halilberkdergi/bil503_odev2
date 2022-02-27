3f7e81c629f8c6fb818241337167997c4da551ff
halilberkdergi <halilberkdergi@gmail.com>
2022-02-28 00:52:06 +0300

    ilk soru bitti. main düzenlenecek...

--- Q1/Segregation/Experiment.java
+++ Q1/Segregation/Experiment.java
@@ -2,6 +2,8 @@ package Segregation;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -16,7 +18,6 @@ public class Experiment {
 
     private List<Agent> unsatisfied;
 
-    Random rand = new Random();
     private  BufferedImage image;
 
 
@@ -46,29 +47,22 @@ public class Experiment {
 
     private void generateTypeTwo(){
         int type0=0,type1=0;
-        int randomNumber;
+        List<Integer> types = new ArrayList<>();
+        for (int x = 0; x < 15000; x++) {
+            types.add(1);
+        }
+        for (int x = 0; x < 15000; x++) {
+            types.add(0);
+        }
+        for (int x = 0; x < 10000; x++) {
+            types.add(2);
+        }
+
+        Collections.shuffle(types);
+
         for (int x = 0; x < gridNumber; x++) {
             for (int y = 0; y < gridNumber; y++) {
-                randomNumber = rand.nextInt(type.getValue() + 1 );
-                switch (randomNumber) {
-                    case 0:
-                        if (type0 < 15000) {
-                            type0++;
-                            break;
-                        } else
-                            randomNumber++;
-                    case 1:
-                        if (type1 < 15000) {
-                            type1++;
-                            break;
-                        } else
-                            randomNumber++;
-                    default:
-                        break;
-                }
-
-
-                map[x][y] = randomNumber;
+                map[x][y] = types.remove(0);
                 updateImage(x, y);
             }
         }
@@ -76,35 +70,25 @@ public class Experiment {
 
     private void generateTypeThree(){
         int type0=0,type1=0,type2=0;
-        int randomNumber;
+        List<Integer> types = new ArrayList<>();
+        for (int x = 0; x < 20000; x++) {
+            types.add(0);
+        }
+        for (int x = 0; x < 10000; x++) {
+            types.add(1);
+        }
+        for (int x = 0; x < 8000; x++) {
+            types.add(2);
+        }
+        for (int x = 0; x < 2000; x++) {
+            types.add(3);
+        }
+
+        Collections.shuffle(types);
+
         for (int x = 0; x < gridNumber; x++) {
             for (int y = 0; y < gridNumber; y++) {
-                randomNumber = rand.nextInt(type.getValue() + 1 );
-                switch (randomNumber) {
-                    case 0:
-                        if (type0 < 15000) {
-                            type0++;
-                            break;
-                        } else
-                            randomNumber++;
-                    case 1:
-                        if (type1 < 10000) {
-                            type1++;
-                            break;
-                        } else
-                            randomNumber++;
-                    case 2:
-                        if (type2 < 5000) {
-                            type2++;
-                            break;
-                        } else
-                            randomNumber++;
-                    default:
-                        break;
-                }
-
-
-                map[x][y] = randomNumber;
+                map[x][y] = types.remove(0);
                 updateImage(x, y);
             }
         }
@@ -119,14 +103,21 @@ public class Experiment {
     }
 
     private boolean isUnsatisfied(Agent agent){
+
         int type = map[agent.x][agent.y];
         if(type == this.type.getValue())
-            return false; // empty grids will be returned satisfied.
-        else{
+            return true; // empty grids will be returned unsatisfied.
+        else {
             int score = 0;
             for (Agent neighbor : agent.getNeighbors()) {
                 if (map[neighbor.x][neighbor.y] == type) score++;
             }
+            /*
+        int type = map[agent.x][agent.y];
+        int score = 0;
+        for (Agent neighbor : agent.getNeighbors()) {
+            if (map[neighbor.x][neighbor.y] == type) score++;
+        }*/
             return score < threshold;
         }
     }
@@ -140,13 +131,50 @@ public class Experiment {
             unsatisfied = findUnSatisfied(unsatisfied);
         }
         System.out.println(unsatisfied.size());
+        List<Integer> types = getTypeList();
+
+        //There are numerous variations in the specific details of how the movement of agents within
+        //a round is handled but all of them produces similar outcomes.
+        Collections.shuffle(types,new Random(3));
+        moveTypes(types);
+
+    }
+
+    private void moveTypes(List<Integer> types) {
+        for(Agent agent : unsatisfied){
+            map[agent.x][agent.y] = types.remove(0);
+            updateImage(agent.x, agent.y);        }
+    }
+
+    private List<Integer> getTypeList() {
+        List<Integer> types = new ArrayList<>();
+        for(Agent agent: unsatisfied)
+        {
+            types.add(map[agent.x][agent.y]);
+        }
+        return types;
     }
 
     private List<Agent> findUnSatisfied(List<Agent> prevUnhapppy) {
+        List<Agent> unsatisfied = new ArrayList<>();
+        for(Agent previous : prevUnhapppy){
+            if(isUnsatisfied(previous))
+                unsatisfied.add(previous);
+
+        }
+        return unsatisfied;
     }
 
     private List<Agent> findUnSatisfied() {
-
+        List<Agent> unsatisfied = new ArrayList<>();
+        for(int x=0 ; x < gridNumber; x++){
+            for (int y=0 ; y < gridNumber ; y++){
+                Agent agent = new Agent(x, y, gridNumber);
+                if(isUnsatisfied(agent))
+                    unsatisfied.add(agent);
+            }
+        }
+        return  unsatisfied;
     }
 
     private Color[] generateColors (Characteristic type){
--- Q1/main/Main.java
+++ Q1/main/Main.java
@@ -1,12 +1,29 @@
 package main;
 
+import Segregation.Characteristic;
+import Segregation.Experiment;
+
 import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
+import java.text.ParseException;
 
 public class Main {
 
-    public static void main(String[] args) {
-	System.out.println("hello world!");
+    public static void main(String[] args) throws InterruptedException, IOException, ParseException {
+
+        Experiment experiment = new Experiment(200,4, Characteristic.TwoTypes);
+        experiment.generate();
+
+        for (int i = 0; i < 50; i++) {
+            if(i%2 == 0){
+                File f = new File("images/" + String.format("%03d", i) + ".png");
+                f.mkdirs();
+                ImageIO.write(experiment.getImage(), "PNG", f);
+
+            }
+            experiment.Step();
+
+        }
     }
 }
